b24e2ef3683c27ced8c838949d83aaaafb9f135e
bhavika-g01 <bhavika.gopalani@nexxbase.com>
2025-07-07 18:18:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,35 @@ import os
 
 app = FastAPI()
 app.mount("/.well-known", StaticFiles(directory=".", html=True), name="static")
-TENANT_ACCESS_TOKEN = "t-g10477fL2IQWIESOEDPKVD46KGONNJ5SLSUCD243"
-SHEET_TOKEN = "Y4ZbwOKXBiO86UkEPwhcRyxBn0g"
+TENANT_ACCESS_TOKEN = "t-g10477kLUF2P4PIQZ272AER64CUOHIRMJ6M57RN2"
+SHEET_TOKEN = "QSMTsNDDEhvm00txBpycTG6ZnBh"
 
-@app.get("/read_feishu_sheet")
-def read_feishu_sheet():
-    url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{SHEET_TOKEN}/values?range=Order_delivery_information!A1:Z100"
+# @app.get("/read_feishu_sheet")
+# def read_feishu_sheet():
+#     url = f"https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{SHEET_TOKEN}/"
+#     headers = {
+#         "Authorization": f"Bearer {TENANT_ACCESS_TOKEN}"
+#     }
+
+#     response = requests.get(url, headers=headers)
+
+#     content_type = response.headers.get("Content-Type", "")
+#     if "application/json" in content_type:
+#         return response.json()
+#     else:
+#         return JSONResponse(
+#             status_code=500,
+#             content={"error": "Non-JSON response", "raw": response.text}
+#         )
+@app.get("/openapi.yaml", include_in_schema=False)
+def get_openapi_yaml():
+    filepath = os.path.join(os.path.dirname(__file__), "openapi.yaml")
+    return FileResponse(filepath, media_type="application/yaml")
+
+@app.get("/read_feishu_doc")
+def read_feishu_doc():
+    doc_id = "WuApw0ktqipZsHk7JIrctRtrnef"
+    url = f"https://open.feishu.cn/open-apis/docx/v1/documents/{doc_id}/content"
     headers = {
         "Authorization": f"Bearer {TENANT_ACCESS_TOKEN}"
     }
@@ -29,7 +52,3 @@ def read_feishu_sheet():
             status_code=500,
             content={"error": "Non-JSON response", "raw": response.text}
         )
-@app.get("/openapi.yaml", include_in_schema=False)
-def get_openapi_yaml():
-    filepath = os.path.join(os.path.dirname(__file__), "openapi.yaml")
-    return FileResponse(filepath, media_type="application/yaml")
